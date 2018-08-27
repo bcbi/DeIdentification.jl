aa3a8f48d6568b7cac91cb0d2fd6009662404221
paulstey <paulstey@gmail.com>
2018-08-27 11:46:11 -0400

    working on new id generation

--- src/hash_columns.jl
+++ src/hash_columns.jl
@@ -1,5 +1,28 @@
 
 
+
+
+function id_generation(hashed_ids, id_dict)
+    n = length(hashed_ids)
+    res = Array{Union{Int, Missing}, 1}(undef, n)
+    for i = 1:n
+        if ismissing(hashed_ids[i])
+            res[i] = missing
+        else
+            if haskey(id_dict, hashed_ids[i])
+                res[i] = id_dict[hashed_ids[i]]
+            else
+                new_id = 1 + length(id_dict)
+                id_dict[hashed_ids[i]] = new_id
+            end
+        end
+    end
+    res
+end
+
+
+
+
 """
     hash_column!(df, col_name, salt_dict)
 This function is used to salt and hash columns containing unique identifiers. Hashing is done in place using SHA256 and a 64-bit salt. Of note is that missing values are left missing. Also note that if `salt_dict` is passed, the method dispatched assumes we are hashing _and_ salting.
@@ -40,11 +63,35 @@ function hash_column!(df, col_name::Symbol)
 end
 
 
+function hash_column!(df, col_name::Symbol, id_dict::Dict{String, Int})
+    n = nrow(df)
+    res = Array{Union{String, Missing}, 1}(n)
+
+    for i = 1:n
+        if ismissing(df[i, col_name])
+            continue
+        else
+            plaintext = string(df[i, col_name])
+            res[i] = hexdigest("sha256", plaintext)
+        end
+    end
+
+    df[:, col_name] = res
+
+    return nothing
+end
+
+
 """
     hash_all_columns!(df, salt_dict, hash_col_names, salt_col_names)
 This function simply iterates over `hash_col_names` and hashes and salts the content in the columns that are specified.
 """
-function hash_all_columns!(df, hash_col_names::Array{Symbol, 1}, salt_col_names::Array{Symbol, 1}, salt::String)
+function hash_all_columns!(df::DataFrame,
+                           hash_col_names::Array{Symbol, 1},
+                           salt_col_names::Array{Symbol, 1},
+                           id_cols::Array{Symbol, 1},
+                           id_dicts::Array{Symbol, 1},
+                           salt::String)
 
     for col in hash_col_names
         hash_column!(df, col)
