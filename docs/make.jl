e80031c8d9889d62634995edca7ff7607914e88f
paulstey <paulstey@gmail.com>
2018-10-29 19:39:34 -0400

    fix typos in package name

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs()
 
 deploydocs(
     deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4"),
-    repo = "github.com/bcbi/DeIdenfication.jl.git",
-    julia  = "1.0",
+    repo = "github.com/bcbi/DeIdentification.jl.git",
+    julia  = "0.7",
     osname = "linux"
 )
