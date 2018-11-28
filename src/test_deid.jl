e066ae57f69728ac1a63e9f1ed70cf66ff0c9bc8
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2018-11-28 18:53:54 -0500

    fix docstring on test_deid

--- src/test_deid.jl
+++ src/test_deid.jl
@@ -3,7 +3,7 @@ using DeIdentification
 using CSV
 using DataFrames
 
-'''
+"""
 Function assumes that you have CSV files for identifiable and deidentified data in
 separate directories. If this is not the case, please move the files accordingly
     --> PATH/identifiable
@@ -13,9 +13,9 @@ If you need to create deidentified data, use make_deid() in make_deid.jl
 This function reuturns two similar dictionaries:
     df_dict - key is identifiable DataFrame, value is matching deidentified DataFrame
     orig2deid - key is identifiable CSV path, value is matching deidentified CSV path
-'''
+"""
 function grab_dfs()
-    
+
     data_dir = "/Users/stephendove/Documents/Lifespan/edsn/DeIdentification/data"
     original_files = readdir(data_dir)
     output_dir = "/Users/stephendove/Documents/Lifespan/edsn/DeIdentification/output"
@@ -33,13 +33,13 @@ function grab_dfs()
                 df1 = CSV.File(data_dir * "/" * orig) |> DataFrames.DataFrame
                 df2 = CSV.File(output_dir * "/" * deid) |> DataFrames.DataFrame
                 df_dict[df1] = df2
-            end 
+            end
         end
     end
 
     return df_dict,orig2deid
 
 end
- 
+
 
 println(df_dict)
