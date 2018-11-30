84537b47509263a7388976135ca4c66495b71666
autodocs <autodocs>
2018-11-30 14:55:11 +0000

    build based on 35344bc

---
+++ dev/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#.-DeIdentification.jl-1",
+    "page": "Home",
+    "title": "1. DeIdentification.jl",
+    "category": "section",
+    "text": "A Julia package for de-identifying data sets"
+},
+
+{
+    "location": "documentation/#",
+    "page": "Manual",
+    "title": "Manual",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "documentation/#.-DeIdentification.jl-1",
+    "page": "Manual",
+    "title": "1. DeIdentification.jl",
+    "category": "section",
+    "text": ""
+},
+
+]}
