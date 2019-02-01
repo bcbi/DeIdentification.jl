d70f4b90e59eba33924cc4daede84e5f608eca55
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-02-01 18:17:50 -0500

    feat: stream data files and deidentify
    remove dataframe intermediary to reduce memory impact for large files

--- src/exporting.jl
+++ src/exporting.jl
@@ -1,37 +1,24 @@
 
-
-import Base.write
-
-
 """
-    write(deid)
+    write_dicts(deid_dicts)
 
 Writes DeIdentified structure to file. The datasets are outputted as CSVs,
 the dictionaries are written to josn. The files are written to the  `output_path`
 specified in the configuration YAML.
 """
-function write(deid::DeIdentified)
-    outdir = deid.deid_config.outdir
-    Memento.info(deid.logger, "$(Dates.now()) ==================== Writing DeIdenfied struct ====================")
-    for (i, df) in enumerate(deid.df_array)
-        df_name = deid.deid_config.df_configs[i].name
-        filename = joinpath(outdir, "deid_" * df_name * ".csv")
-        Memento.info(deid.logger, "$(Dates.now()) Writing de-identified dataframe $(df_name) to $(filename)")
-        CSV.write(filename, df.df)
-    end
+function write_dicts(deid::DeIdDicts, logger, outdir)
+    
+    idfile = joinpath(outdir, "id_dicts_" * string(Dates.now()) * ".json")
+    Memento.info(logger, "$(Dates.now()) Writing ID dictionaries to $(idfile)")
+    write(idfile, JSON.Writer.json(deid.id, 4))
 
+    dateshift_file = joinpath(outdir, "dateshifts_" * string(Dates.now()) * ".json")
+    Memento.info(logger, "$(Dates.now()) Writing dateshift values to $(dateshift_file)")
+    write(dateshift_file, JSON.Writer.json(deid.dateshift, 4))
 
-    idfile = joinpath(outdir, "id_dicts.json")
-    Memento.info(deid.logger, "$(Dates.now()) Writing ID dictionaries to $(idfile)")
-    write(idfile, JSON.Writer.json(deid.id_dicts, 4))
-
-    dateshift_file = joinpath(outdir, "dateshifts.json")
-    Memento.info(deid.logger, "$(Dates.now()) Writing dateshift values to $(dateshift_file)")
-    write(dateshift_file, JSON.Writer.json(deid.dateshift_dict, 4))
-
-    saltfile = joinpath(outdir, "salts.json")
-    Memento.info(deid.logger, "$(Dates.now()) Writing salt values to $(saltfile)")
-    write(saltfile, JSON.Writer.json(deid.salt_dict, 4))
+    saltfile = joinpath(outdir, "salts_" * string(Dates.now()) * ".json")
+    Memento.info(logger, "$(Dates.now()) Writing salt values to $(saltfile)")
+    write(saltfile, JSON.Writer.json(deid.salt, 4))
 
     return nothing
 end
